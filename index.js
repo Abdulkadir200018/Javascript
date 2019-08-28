26fe1ca84cb3f5599702d764e6cf95f3f18cb381
Abdulkadir200018 <53016898+Abdulkadir200018@users.noreply.github.com>
2019-08-28 20:02:29 +0300

    Add files via upload
    I have done my first javascript1

---
+++ index.js
@@ -0,0 +1,98 @@
+document.write("Hello Addunyaa")
+console.log('I,m awesome');
+var x;
+console.log('the value of my variable x will be: whateverYouThinkItWillLog');
+console.log('whole number');
+x = 12;
+console.log('the value of my variable x will be: whateverYouThinkItWillLog');
+console.log('whole number');
+
+let y = "abel";
+console.log('the value of my variable y will be: ehtiopian men name');
+console.log('y');
+y = "chala";
+console.log('the value of my variable y will be: ehtiopian men name');
+console.log('y');
+
+const z = 7.25;
+console.log('y');
+const a = 7;
+console.log('a');
+if (7.25>=7){
+    console.log( 'm')
+
+}else{ console.log ("h")}
+console.log(7.25)
+
+//Arrays!
+const Animals = []
+console.log('I would like to list wild animals')
+console.log(Animals)
+const wildanimals = ['zebra', 'lion', 'cheettah', 'elephant']
+console.log(wildanimals)
+console.log(wildanimals.push("baby pig"));
+console.log(wildanimals)
+
+
+
+let myString = "this is a test"
+//console.log(myString.unshift("it is a best to practice"));
+console.log(myString);
+console.log(myString.length);
+
+var A = "7";
+const B = true;
+let C = "table";
+const D = {
+    name: "Abdii",
+    age:30,
+    address: "Athens"
+};
+var F = "chairs"
+console.log(A);
+console.log(B);
+console.log(C);
+console.log(D);
+console.log(F);
+let E = 7;
+console.log('the number of woman in the car is' + E);
+console.log(typeof A);
+console.log(typeof B);
+console.log(typeof C);
+console.log(typeof D);
+console.log(typeof E);
+console.log(typeof F);
+if (A===B){
+    console.log( 'Same Type')
+}else{ console.log ("Not Same")}
+if (A===C){
+    console.log( 'Same Type')
+}else{ console.log ("Not Same")}
+if (A===D){
+    console.log( 'Same Type')
+}else{ console.log ("Not Same")}
+if (A===E){
+    console.log( 'Same Type')
+}else{ console.log ("Not Same")}
+if (C===F){
+    console.log( 'Same Type')
+}else{ console.log ("Not Same")}
+
+console.log('7 divided by 3 which gives a result 2 and remainder 1 ')
+const myhome = [4, 'fati', 'nur', 66, 'ali'];
+console.log(myhome);
+let g = 6/0;
+let r = 10/0;
+if (g===r){
+    console.log( 'Equal')
+}else{ console.log ("Not Equal")}
+console.log(g);
+console.log(r);
+
+
+
+
+
+
+
+
